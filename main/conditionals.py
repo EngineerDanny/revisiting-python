73151012d3237eb7a7203b83f45277f874b02e44
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-15 12:18:29 -0400

    conditional statements

---
+++ main/conditionals.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you : ")) 
+
+if age >= 18 and age <290:
+  print("You are an adult")
+elif age>0:
+  print("You are a child")
+else:
+  print("You havent been born oops🥵")
+  
