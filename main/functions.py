4808e5cc0e91381b027e34b4c19df6d66a581840
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-21 09:28:56 -0400

    more functions

---
+++ main/functions.py
@@ -0,0 +1,17 @@
+#This is a function with no arguments
+def function_one():
+ print('This is a new function')
+
+function_one()
+
+#This is a function with arguments
+
+def function_two(args):
+ if args is str:
+    print('Argument is a string')
+ else:   
+    print('Argument is not a string')
+ 
+
+function_two("asdf")
+
---
+++ main/import_functions.py
@@ -0,0 +1,4 @@
+from functions import function_one
+
+
+function_one()
