356b0257e7e7d6a81b2c1c2333a651758c6d73ca
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-26 14:25:37 -0400

    more stuffs added to collections file

---
+++ main/collections.py
@@ -0,0 +1,23 @@
+# Collections can be divided into :
+# 1. tuples which is ordered
+m_tuple = ("Danny" ,  21 , "Male")
+
+for item in m_tuple:
+    print(item)
+print("\n", end="")
+
+# 2. Sets which is unordered
+m_sets = {"Suzzy" , 18 , "Female"}
+
+for item in m_sets:
+    print(item)
+
+#3. Dictionary is an unordered form of unique key value pairs
+m_dict = {
+    "name" : "Danny",
+    "age" :19,
+    "gender" : "Male"
+}
+
+print(m_dict)
+
