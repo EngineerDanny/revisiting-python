15d1b4317f7368854c14c0e6a69ed936ce411b5c
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-26 14:25:54 -0400

    add types of parameters in a function

--- main/functions.py
+++ main/functions.py
@@ -3,7 +3,7 @@ def function_one():
    
  print('This is a new function')
 
-function_one()
+# function_one()
 
 #This is a function with arguments
 
@@ -14,5 +14,30 @@ def function_two(args):
     print('Argument is not a string')
  
 
-function_two("asdf")
+# function_two("asdf")
 
+
+
+# function with positional parameters and named parameters
+# optional parameters are those that are not required by the user to provide
+
+def para_fun(name,age):
+    print(name + " "+ str(age))
+
+para_fun( name= "Danny", age= 90) #This is a named argument
+para_fun( "Danny", 90) #This is a postional argument
+
+# *args is a parameter that contains tuple that has a list of varying arguments
+# could be any variable name in place of the args
+def sum(*numbers):
+   total = 0
+   for number in numbers:
+      total+=number
+   print("The sum of the numbers is " + str(total) )
+
+
+
+
+
+sum(5,90,212,2434,54,23,12,32,54,65,76)   
+    
