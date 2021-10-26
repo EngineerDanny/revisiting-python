2336cc927c63bc0d25208303c08b9373b45b6bac
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-26 09:56:25 -0400

    create a table with nested loops

---
+++ main/nested_loops.py
@@ -0,0 +1,11 @@
+
+
+
+rows = int(input("How many rows? "))
+colums = int(input("How many columns? "))
+
+##outer for loops
+for row in range(0,rows):
+    for col in range(0, colums):
+      print('col' , end=" ")
+    print("\n")  
