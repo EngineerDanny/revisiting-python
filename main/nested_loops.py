74db852f73a362d83da897dc00c125ec2908f122
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-26 09:57:46 -0400

    updates in the nested_loops

--- main/nested_loops.py
+++ main/nested_loops.py
@@ -3,9 +3,10 @@
 
 rows = int(input("How many rows? "))
 colums = int(input("How many columns? "))
+symbol = input("What symbol do you want to use")
 
 ##outer for loops
 for row in range(0,rows):
     for col in range(0, colums):
-      print('col' , end=" ")
+      print(symbol , end=" ")
     print("\n")  
