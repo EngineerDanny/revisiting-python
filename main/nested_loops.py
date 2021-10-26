32002750193e34c4ba77df350f5865ad01aeb3ee
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-26 14:25:00 -0400

    comment out the nested loops code

--- main/nested_loops.py
+++ main/nested_loops.py
@@ -10,3 +10,9 @@ for row in range(0,rows):
     for col in range(0, colums):
       print(symbol , end=" ")
     print("\n")  
+
+#control of flow in loops
+### break : Used to terminate from the loop entirely
+### continue : Used to skip to the next iteration of the loop if it exists
+### pass : does nothing but acts as a placeholder
+
