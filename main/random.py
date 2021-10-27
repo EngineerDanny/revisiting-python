de6aa337d6d571ee9db4ac10561ff86d260e75a1
EngineerDanny <agyapongdaniel7777@gmail.com>
2021-10-27 16:36:41 -0400

    utilizing the random package

---
+++ main/random.py
@@ -0,0 +1,17 @@
+#exploring random python number generator
+
+import random
+
+
+# This prints a series of random numbers
+for item in range(1,5):
+    value = random.randint(1*10^10,2*9)
+    # print(value)
+
+food = ['Pizza' , 'Burger', 'Doughnut', 'Cheese'] 
+# rand_food = random.choice(food)
+shuffled_food = random.shuffle(food)
+print(shuffled_food)
+
+
+
